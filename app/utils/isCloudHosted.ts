aa3a492b9d4a27d91753dd19bc92c768a1b275dd
Jaden Wang <Jaden.Shiteng.Wang@gmail.com>
2024-01-02 08:07:35 -0500

    removed isCloudHosted limitation

--- app/utils/isCloudHosted.ts
+++ app/utils/isCloudHosted.ts
@@ -3,7 +3,7 @@ import env from "~/env";
 /**
  * True if the current installation is the cloud hosted version at getoutline.com
  */
-const isCloudHosted = [
+const isCloudHosted = true || [
   "https://app.getoutline.com",
   "https://app.outline.dev",
   "https://app.outline.dev:3000",
